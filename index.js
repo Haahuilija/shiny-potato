7c115a30ce92f23e8c22aeb5415f6bd041c60d6f
SaulK <saul.koskinen@gmail.com>
2023-05-06 22:01:41 +0300

    Starting fresh with cloud run

--- index.js
+++ index.js
@@ -1,48 +1,44 @@
-// Copyright 2021 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+const express = require('express');
+const {readFileSync} = require('fs');
+const handlebars = require('handlebars');
 
-const app = require('./app');
-const {logger, initLogCorrelation} = require('./utils/logging');
-const {fetchProjectId} = require('./utils/metadata');
+const app = express();
+// Serve the files in /assets at the URI /assets.
+app.use('/assets', express.static('assets'));
 
-/**
- * Initialize app and start Express server
- */
-const main = async () => {
-  let project = process.env.GOOGLE_CLOUD_PROJECT;
-  if (!project) {
+// The HTML content is produced by rendering a handlebars template.
+// The template values are stored in global state for reuse.
+const data = {
+  service: process.env.K_SERVICE || '???',
+  revision: process.env.K_REVISION || '???',
+};
+let template;
+
+app.get('/', async (req, res) => {
+  // The handlebars template is stored in global state so this will only once.
+  if (!template) {
+    // Load Handlebars template from filesystem and compile for use.
     try {
-      project = await fetchProjectId();
-    } catch (err) {
-      logger.warn('Could not fetch Project Id for tracing.');
+      template = handlebars.compile(readFileSync('index.html.hbs', 'utf8'));
+    } catch (e) {
+      console.error(e);
+      res.status(500).send('Internal Server Error');
     }
   }
-  // Initialize request-based logger with project Id
-  initLogCorrelation(project);
 
-  // Start server listening on PORT env var
-  const PORT = process.env.PORT || 8080;
-  app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
-};
-
-/**
- * Listen for termination signal
- */
-process.on('SIGTERM', () => {
-  // Clean up resources on shutdown
-  logger.info('Caught SIGTERM.');
-  logger.flush();
+  // Apply the template to the parameters to generate an HTML string.
+  try {
+    const output = template(data);
+    res.status(200).send(output);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-main();
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(
+    `Hello from Cloud Run!! The container started successfully and is listening for HTTP requests on ${PORT}`
+  );
+});
--- test/system.test.js
+++ test/system.test.js
@@ -1,41 +1,22 @@
-// Copyright 2021 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-'use strict';
-
 const assert = require('assert');
-const got = require('got');
+const {request} = require('gaxios');
 
-describe('System Tests', () => {
-  const {BASE_URL, ID_TOKEN} = process.env;
+const port = process.env.PORT || '8080';
+const url = process.env.SERVICE_URL || `http://localhost:${port}`;
+const token = process.env.TOKEN || '';
 
-  before(async () => {
-    if (!BASE_URL) throw Error('Cloud Run service URL not found');
-    if (!ID_TOKEN) throw Error('Unable to acquire an ID token.');
-  });
+describe('Hello World', () => {
+  it('can respond to an HTTP request', async () => {
+    console.log(`    - Requesting GET ${url}/...`);
+    const res = await request({
+      url: url + '/',
+      headers: {"Authorization": "Bearer " + token},
+      timeout: 5000,
+    });
 
-  it('can successfully make a request', async () => {
-    const options = {
-      headers: {
-        Authorization: `Bearer ${ID_TOKEN}`,
-      },
-      method: 'GET',
-      throwHttpErrors: false,
-      retry: 6,
-    };
-    const response = await got(BASE_URL, options);
-    assert.strictEqual(response.statusCode, 200);
-    assert.strictEqual(response.body, 'Hello World!');
+    assert.equal(res.status, '200');
+    assert.ok(
+      res.data.includes('Congratulations, you successfully deployed a container image to Cloud Run'),
+    );
   });
 });
